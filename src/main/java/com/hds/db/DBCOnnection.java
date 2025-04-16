2efe9bdf9c6c321517f80f9d070a54c40cf9f393
HShayy <hshaikh831@live.seminolestate.edu>
2025-04-16 07:50:50 -0400

    Fixed Products link in index.html

--- src/main/java/com/hds/db/DBCOnnection.java
+++ src/main/java/com/hds/db/DBCOnnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBCOnnection {
 
     // Update with your actual database name, username, and password.
-	private static final String URL = "jdbc:mysql://host.docker.internal:3306/hds_database";
+	private static final String URL = "jdbc:mysql://host.docker.internal:3306/hds_database?useUnicode=true&characterEncoding=UTF-8";
 	private static final String USER = "root";
 	private static final String PASSWORD = "Monster2@";
   
